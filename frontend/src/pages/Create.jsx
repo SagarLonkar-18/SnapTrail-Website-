6ef72fb6a9787a12dfc6f832d3623de8ff1aa32a
SagarLonkar-18 <sagarlonkar75@gmail.com>
2025-04-07 23:35:25 +0530

    image upload on create page

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { FaPlus } from 'react-icons/fa';
 
 const Create = () => {
+
+    const inputRef = useRef(null);
+
+    const handleClick = () => {
+        inputRef.current.click();
+    }
+
+    const [file, setFile] = useState("");
+    const [filePrev, setFilePrev] = useState("")
+
+    const changeFileHandler = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onloadend = () => {
+            setFilePrev(reader.result);
+            setFile(file);
+        }
+    }
+
     return (
-        <div>Create</div>
+        <div className='flex flex-wrap justify-center items-center gap-2 mt-10'>
+            <div className='flex items-center justify-center'>
+                <div className='flex flex-col items-center justify-center w-80 h-auto p-6 bg-white rounded-lg shadow-lg'>
+                    {
+                        filePrev && <img src={filePrev} alt=""/>
+                    }
+                    <div className='flex flex-col items-center justify-center h-full cursor-pointer' onClick={handleClick}>
+                        <input type="file" accept='image/*' className='hidden' ref={inputRef} onChange={changeFileHandler}/>
+                        <div className='w-12 h-12 mb-4 flex items-center justify-center bg-gray-200 rounded-full'>
+                            <FaPlus/>
+                        </div>
+                        <p className='text-gray-500'>Choose a file</p>
+                    </div>
+                    <p className='mt-4 text-sm text-gray-400'>we recommend using high quality .jpg but less than 10MB</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
